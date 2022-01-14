8636575b89645dd5aa5a430cee1e23d2be1a7023
Chicco Moshe Serby <chiccomoshe@gmail.com>
2022-01-14 15:21:42 +0200

    props types

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Square from "../Square/Square";
 import { StyledBoard } from "./Board.style";
 
 const Board = ({ squares, onSquareClick }) => {
-
   return (
-      <StyledBoard>
-        {squares?.map((singleSquare, idx) => (
-          <Square val={singleSquare} key={idx} onSquareClick={() => onSquareClick(idx)} />
-        ))}
-      </StyledBoard>
+    <StyledBoard>
+      {squares?.map((singleSquare, idx) => (
+        <Square
+          val={singleSquare}
+          key={idx}
+          onSquareClick={() => onSquareClick(idx)}
+        />
+      ))}
+    </StyledBoard>
   );
 };
 
+Board.propTypes = {
+  squares: PropTypes.array,
+  onSquareClick: PropTypes.func,
+  content: PropTypes.any,
+  extraStyling: PropTypes.object,
+};
+
+Board.defaultProps = {
+  squares: [],
+  onSquareClick: () => {},
+};
 export default Board;
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,8 +1,8 @@
-import React  from "react";
+import React from "react";
 import { StyledSquare } from "./Square.style";
 
-const Square = ({ val = 'X', onSquareClick = ()=>{}, disabled= false }) => {
-  const color = val === 'X' ? 'red' : 'blue';
+const Square = ({ val = "X", onSquareClick = () => {}, disabled = false }) => {
+  const color = val === "X" ? "red" : "blue";
   return (
     <StyledSquare color={color} onClick={onSquareClick} disabled={disabled}>
       {val}
